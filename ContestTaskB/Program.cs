0466cbeb7b50bacfdd64da24094f963439433ef4
Андрей Беляков <ttxiom@gmail.com>
2020-09-07 20:07:28 +0500

    TaskB

---
+++ ContestTaskB/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContestTaskB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Int32.Parse(Console.ReadLine());
+            char c = 'X';
+
+            string s = new String(c, n);
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(s);
+            }
+
+            Console.ReadLine();
+            /*
+            XXXX
+            XX X
+            X XX
+            XXXX
+            */
+
+        }
+    }
+}
